930f802869b98e9358c649abe583471d50a29e81
Kory Malmrose <kmalmrose@student.neumont.edu>
2013-04-15 16:49:44 -0600

    More Extraction of Classes
    New Player classes, implementing an interface, so the game can create
    players of any kind. They're not implemented yet.

---
+++ ComputerPlayer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nim
+{
+    class ComputerPlayer : IPlayer
+    {
+        private String _name;
+        private Random rand;
+        public ComputerPlayer(String name){
+            _name = name;
+            rand = new Random();
+        }
+        public bool makeMove(GameManager game)
+        {
+            int bestMove = -1;
+            for (int i = 0; i < game.stateList.Count; i++)
+            {
+                if (game.stateList[i].TopRow <= game.rows[0] &&
+                    game.stateList[i].MidRow <= game.rows[1] &&
+                    game.stateList[i].BotRow <= game.rows[2]){
+                    int numRowsToMove = 0;
+                    if (game.stateList[i].TopRow < game.rows[0])
+                    {
+                        numRowsToMove++;
+                    }
+                    if (game.stateList[i].MidRow < game.rows[1])
+                    {
+                        numRowsToMove++;
+                    }
+                    if (game.stateList[i].BotRow < game.rows[2]){
+                        numRowsToMove++;
+                    }
+                    if (numRowsToMove == 1){
+                        if (bestMove == -1){
+                            bestMove = i;
+                        }
+                        else if (game.stateList[i].getWeight() <= game.stateList[bestMove].getWeight()){
+                            bestMove = i;
+                        }
+                    }
+                }
+            }
+            if (bestMove != -1 && game.stateList.Count > 0){
+                game.rows[0] = game.stateList[bestMove].TopRow;
+                game.rows[1] = game.stateList[bestMove].MidRow;
+                game.rows[2] = game.stateList[bestMove].BotRow;
+                game.moveMade();
+            }else{
+                bool hasMoved = false;
+                while (!hasMoved){
+                    int randRow = rand.Next(3);
+                    if (game.rows[randRow] > 0){
+                        int numRemoved = rand.Next(game.rows[randRow] - 1);
+                        game.rows[randRow] -= numRemoved + 1;
+                        hasMoved = true;
+                        game.moveMade();
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ HumanPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nim
+{
+    class HumanPlayer : IPlayer
+    {
+        private String _name;
+        public HumanPlayer(String name){
+            _name = name;
+        }
+        public bool makeMove(GameManager game)
+        {
+            bool moveMade = false;
+            while (!moveMade)
+            {
+                game.printBoard();
+                Console.WriteLine("Choose number of row");
+                Console.WriteLine("(0) for First Row, (1) for Second Row, (2) for Third Row");
+                string rowNumber = Console.ReadLine();
+                int row = int.Parse(rowNumber);
+
+                Console.WriteLine("How many pieces do you want to take away?");
+                string takePieces = Console.ReadLine();
+                int num = int.Parse(takePieces);
+                if (!game.makeMove(row, num))
+                {
+                    Console.WriteLine("Invalid Input, try again");
+                }
+                else
+                {
+                    moveMade = true;
+                }
+            }
+            return true;
+        }
+    }
+}
